6bdea13b8edbb7a53d8f88ff8a5de6ca7608cf17
Walter Satriano <walter.satri@gmail.com>
2019-09-11 17:14:08 +0200

    provo ad associare i vari "access" ma non funziona

--- fulldb3.php
+++ fulldb3.php
@@ -4,6 +4,25 @@
 
   include "database3.php";
 
+  // $level = $_GET["level"];
+  //
+  // $result = [];
+  //
+  // if ($level == "guest" && $data["access"] == "guest") {
+  //   $result[] = $data["fatturato"];
+  // }
+  //
+  // if ($level == "employee" && $data["access"] == "employee") {
+  //   $result[] = $data["fatturato"];
+  //   $result[] = $data["fatturato_by_agent"];
+  // }
+  //
+  // if ($level[] == "clevel" && $data["access"] == "clevel") {
+  //   $result[] = $data;
+  // }
+
+
+
   echo json_encode($data);
 
 
