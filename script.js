8169253f4d023ec07f33efd2e5d3504ada7cc639
Walter Satriano <walter.satri@gmail.com>
2019-09-11 12:14:29 +0200

    step 2 ok

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
 
-  getData();
+  getDataStep1();
+  getDataStep2();
 
 
+  //funzione per convertire la data in italiano
+  function getMonths() {
+
+    moment.locale("it");
+    var months = moment.months();
+
+    return months;
+  }
+
+  //--------------------- STEP 1 ---------------------------
   //funzione per prendere i dati
-  function getData() {
+  function getDataStep1() {
 
     $.ajax({
 
@@ -12,35 +23,21 @@ $(document).ready(function() {
       method: "GET",
       success: function(data) {
 
-        printLineGraph(data);
-        console.log("data", data);
-
+        printLineGraphStep1(data);
+        console.log("data1", data);
       },
       error: function() {
-
         alert("errore!!!");
       }
     });
   }
 
-  //funzione per convertire la data in italiano
-  function getMonths() {
-
-    moment.locale("it");
-
-    var months = moment.months();
-    console.log("funzione getMonths", months);
-
-    return months;
-  }
-
-
   //funzione per stampare il grafico a linea
-  function printLineGraph(data) {
+  function printLineGraphStep1(data) {
 
     var monthsLabel = getMonths();
 
-    var ctxLine = document.getElementById("line_graph").getContext("2d");
+    var ctxLine = $("#line_graph_montly_sales");
     var lineGraph = new Chart(ctxLine, {
       type: "line",
       data: {
@@ -55,6 +52,79 @@ $(document).ready(function() {
       },
     });
   }
+
+
+  // ------------------- STEP 2 -------------------------
+  //funzione per prendere i dati
+  function getDataStep2() {
+
+    $.ajax({
+
+      url: "fulldb2.php",
+      method: "GET",
+      success: function(data) {
+
+        printLineGraphStep2(data);
+        printPieGraphStep2(data);
+        console.log("data2", data);
+      },
+      error: function() {
+        alert("errore!!!");
+      }
+    });
+  }
+
+  //funzione per stampare il grafico a linea
+  function printLineGraphStep2(data) {
+
+    var monthsLabel = getMonths();
+    var typeGraph = data.fatturato.type;
+    var montlySales = data.fatturato.data;
+
+    var ctxLine = $("#line_graph_montly_sales2");
+    var lineGraph = new Chart(ctxLine, {
+      type: typeGraph,
+      data: {
+        labels: monthsLabel,
+        datasets: [{
+          label: "# Vendite",
+          data: montlySales,
+          borderColor: [
+            "rgb(170, 0, 255)"
+          ]
+        }]
+      },
+    });
+  }
+
+  //funzione per stampare il grafico a torta
+  function printPieGraphStep2(data) {
+
+    var typeGraph = data.fatturato_by_agent.type;
+    var agentName = Object.keys(data.fatturato_by_agent.data);
+    var salesByAgent = Object.values(data.fatturato_by_agent.data);
+
+    var ctxPie = $("#pie_graph_sales_by_agent");
+    var pieGraph = new Chart(ctxPie, {
+      type: typeGraph,
+      data: {
+        labels: agentName,
+        datasets: [{
+          label: "# Vendite per Agente",
+          data: salesByAgent,
+          backgroundColor: [
+            "rgb(255, 0, 0)",
+            "rgb(255, 255, 0)",
+            "rgb(0, 255, 0)",
+            "rgb(0, 0, 255)"
+          ]
+        }]
+      },
+    });
+  }
+
+
+
 
 
 });
