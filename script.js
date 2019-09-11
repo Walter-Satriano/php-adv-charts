439c6f649f96cf741845b5d12b2b11a23523a780
Walter Satriano <walter.satri@gmail.com>
2019-09-11 16:02:52 +0200

    aggiunto grafico multilinea step3

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   getDataStep1();
   getDataStep2();
+  getDataStep3();
 
 
   //funzione per convertire la data in italiano
@@ -43,11 +44,10 @@ $(document).ready(function() {
       data: {
         labels: monthsLabel,
         datasets: [{
-          label: "# Vendite",
+          label: "Vendite",
           data: data,
-           borderColor: [
-             "rgb(170, 0, 255)"
-           ]
+          borderColor: "rgb(170, 0, 255)",
+          backgroundColor: "rgb(194, 194, 194, 0.2)"
         }]
       },
     });
@@ -87,11 +87,10 @@ $(document).ready(function() {
       data: {
         labels: monthsLabel,
         datasets: [{
-          label: "# Vendite",
+          label: "Vendite",
           data: montlySales,
-          borderColor: [
-            "rgb(170, 0, 255)"
-          ]
+          borderColor: "rgb(170, 0, 255)",
+          backgroundColor: "rgb(194, 194, 194, 0.2)"
         }]
       },
     });
@@ -119,6 +118,66 @@ $(document).ready(function() {
             "rgb(0, 0, 255)"
           ]
         }]
+      },
+    });
+  }
+
+  // ------------------- STEP 3 -------------------------
+  //funzione per prendere i dati
+  function getDataStep3() {
+
+    $.ajax({
+
+      url: "fulldb3.php",
+      method: "GET",
+      success: function(data) {
+
+        printMultiLineGraph(data);
+        console.log("data3", data);
+      },
+      error: function() {
+        alert("errore!!!");
+      }
+    });
+  }
+
+  //funzione per stampare il grafico multilinea
+  function printMultiLineGraph(data) {
+
+    var monthsLabel = getMonths();
+    var typeGraph = data.team_efficiency.type;
+    var team = Object.keys(data.team_efficiency.data);
+    var efficiencytTeam1 = Object.values(data.team_efficiency.data.Team1);
+    var efficiencytTeam2 = Object.values(data.team_efficiency.data.Team2);
+    var efficiencytTeam3 = Object.values(data.team_efficiency.data.Team3);
+
+    console.log("team", team);
+
+    var ctxLine = $("#multiline_graph_team");
+    var lineGraph = new Chart(ctxLine, {
+      type: typeGraph,
+      data: {
+        labels: monthsLabel,
+        datasets: [
+          {
+            label: team[0],
+            data: efficiencytTeam1,
+            backgroundColor: "rgb(194, 194, 194, 0.2)",
+            borderColor: "rgb(0, 255, 0)"
+          },
+          {
+            label: team[1],
+            data: efficiencytTeam2,
+            backgroundColor: "rgb(194, 194, 194, 0.3)",
+            borderColor: "rgb(0, 0, 255)"
+          },
+          {
+            label: team[2],
+            data: efficiencytTeam3,
+            backgroundColor: "rgb(194, 194, 194, 0.4)",
+            borderColor: "rgb(255, 0, 0)"
+          }
+        ]
       },
     });
   }
